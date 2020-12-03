a90459c2f137fd0f823cca069716fbb15ce6d6cd
Stanislav Ponomarev <chemburleno@yandex.ru>
2020-12-03 11:16:09 +0300

    add cors

--- routes/cors.js
+++ routes/cors.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const cors = require('cors');
 
 const corsOptions = {
-  origin: ['http://localhost:8080', 'http://chelentano.students.nomoreparties.space'],
+  origin: ['http://localhost:8080', 'https://chelentano.students.nomoreparties.space'],
   credentials: true,
   optionsSuccessStatus: 200,
 };
